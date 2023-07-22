d970b9db72476ba47a368613a085e0bb28cd78a8
angu-geetha <angu.geetha@gmail.com>
2023-07-22 01:21:21 -0400

    commit

---
+++ RestAssuredTest/src/test/java/api/model/ProgramBatch.java
@@ -0,0 +1,13 @@
+package api.model;
+
+public class ProgramBatch {
+	
+	public Integer batchId;
+	public String batchName;
+	public String  batchDescription;
+	public String batchStatus;
+	public Integer batchNoOfClasses;
+	public Integer programId;
+    public String programName;
+
+}
